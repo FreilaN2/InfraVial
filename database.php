9f011312310ae432a5ec60d5b7052ba52969ef3e
FreilaN2 <exauserrano24@gmail.com>
2025-06-17 12:11:14 -0400

    Corrección de la BD

--- database.php
+++ database.php
@@ -10,16 +10,3 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 ?>
-
-<?php
-$host = "localhost:3306";
-$user = "root";
-$pass = "";
-$db = "vialbarinas";
-
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-?>
